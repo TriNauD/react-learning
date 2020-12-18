d043ccd094f3a1153f1c71f452560e9aeae278d3
TriNauD <xujingyi2017@bupt.edu.cn>
2020-12-18 17:40:30 +0800

    Return a set of elements(a div component) in Welcome.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Welcome(props) {
-    return <h1>Hello, {props.name}!</h1>
+    return (
+        <div>
+            <h1>Hello, {props.name}!</h1>
+            <h2>Your id is {props.ID}</h2>
+            <h2>Your age is {props.age}</h2>
+        </div>
+    );
 }
 
-let element = <Welcome name='Lisa'/>
+// const user = {
+//     ID: '0001',
+//     studentAge: '17',
+//     studentName: {
+//         firstName: 'Imai',
+//         lastName: 'Lisa',
+//     },
+// }
+
+let element = <Welcome ID='114514' name='Lisa' age='17'/>
+
 ReactDOM.render(
     element,
     document.getElementById('root')
