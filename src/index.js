6ed9a9edb8130e7a15a45ccc85bd919623928b18
TriNauD <xujingyi2017@bupt.edu.cn>
2020-12-18 18:36:35 +0800

    Part of the clock.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+//传入Date() 返回一个div
+function Clock(props) {
+    return (
+        <div>
+            <h1>This is a clock</h1>
+            <h2>It is {props.date.toLocaleString()}</h2>
+        </div>
+    );
+}
+
+//被调用的时候重新render一个新组件 给Clock传参数Date()
+function tick() {
+    ReactDOM.render(
+        <Clock date={new Date()}/>,
+        document.getElementById('root')
+    );
+}
+
+//每1000ms回调一下tick()
+setInterval(tick, 1000);
