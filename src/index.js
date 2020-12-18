d6859735e3d70cf8036a36c5c67279c6505ddce7
TriNauD <xujingyi2017@bupt.edu.cn>
2020-12-18 14:26:08 +0800

    Customize function element <Welcome>.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function Welcome(props) {
+    return <h1>Hello, {props.name}!</h1>
+}
+
+let element = <Welcome name='Lisa'/>
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
