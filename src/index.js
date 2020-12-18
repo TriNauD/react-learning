d9cb0a686498bef900321ea796265cec8e2580a2
TriNauD <xujingyi2017@bupt.edu.cn>
2020-12-18 11:29:36 +0800

    Greet user with JSX.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,33 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function formatName(user) {
+    return (
+        user.props.firstName + ' ' + user.props.lastName
+    );
+}
+
+function greetingUser(user) {
+    if (user.props.firstName != null && user.props.lastName != null) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+    } else {
+        return <h1>Hello, stranger!</h1>;
+    }
+}
+
+const user = {
+    id: 'student1',
+    props: {
+        firstName: 'Imai',
+        lastName: 'Lisa',
+    }
+}
+
+const element = (
+    greetingUser(user)
+);
+
+ReactDOM.render(
+    element,
+    document.getElementById("root")
+);
