bcfaa2e0d6505562a861a7493c76a8c8acba3130
TriNauD <xujingyi2017@bupt.edu.cn>
2020-12-18 13:57:43 +0800

    Tick every 1000ms.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+function tick() {
+    const element = (
+        <div>
+            <h2>It is {new Date().toLocaleString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
